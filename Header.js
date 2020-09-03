1356341bd2554a6c688f747c1c01efe41a6a4c10
SriSravyaN <50413203+SriSravyaN@users.noreply.github.com>
2020-09-03 13:33:26 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+        <img src="https://thumbnail.imgbin.com/20/15/0/imgbin-rage-comic-youtube-know-your-meme-comics-youtube-KAd25ypuy633Db2meVSRGygSk_t.jp"
+        alt="problem?" />
+        <p>Meme Generator</p>
+        
+        </header>
+        
+        
+    )
+}
+
+export default Header
